886ca62a8df0f376c5dd231ccbe2d06498971d98
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2025-02-01 16:57:33 +0200

    finish with header calendar

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import css from './MonthStatsTable.module.css';
 
 const MonthStatsTable = () => {
-  return <div>MonthStatsTable</div>;
+  const currentDate = new Date();
+  const month = currentDate.toLocaleDateString('en-GB', {
+    month: 'long',
+  });
+  const year = currentDate.getFullYear();
+
+  return (
+    <div>
+      <ul className={css.calendarHeader}>
+        <li className={css.headerItem}>
+          <h3 className={css.title}>Month</h3>
+        </li>
+        <li className={css.headerItem}>
+          <button className={css.button} type="button">
+            <svg className={css.icon} width="20" height="20">
+              <use href="/public/images/icons.svg#arrow-calendar-icon"></use>
+            </svg>
+          </button>
+          <div>
+            {month}, {year},
+          </div>
+          <button className={css.button} type="button">
+            <svg className={css.icon} width="20" height="20">
+              <use href="/public/images/icons.svg#arrow-calendar-icon"></use>
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default MonthStatsTable;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <h2>HomePage</h2>
+      <MonthStatsTable />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
