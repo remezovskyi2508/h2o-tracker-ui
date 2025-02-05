ff4f6210bb89109bbd084d7609b1fb083e728800
Olena Parakonna <166116539+Parakonna@users.noreply.github.com>
2025-02-05 19:40:45 +0100

    com

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,41 +1,33 @@
 import { useState } from 'react';
 import { GrAddCircle } from 'react-icons/gr';
 import { IoIosRemove } from 'react-icons/io';
-import ResizableSlider from '../ResizableSlider/ResizableSlider';
 
 import css from './WaterRatioPanel.module.css';
 import TodayListModal from '../TodayListModal/TodayListModal';
 
 const WaterRatioPanel = () => {
   const [waterData, setWaterData] = useState({
-    norma: '1.5L',
     percentage: '40%',
   });
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const norma = parseFloat(waterData.norma) * 1000;
   const percentage = Number(parseFloat(waterData.percentage).toFixed(1)) || 0;
-
-  const step = (250 * 100) / norma;
-
-  const onChange = value => {
-    const roundedValue = Number(value.toFixed(1));
-    setWaterData({
-      ...waterData,
-      percentage: `${roundedValue}%`,
-    });
-    /* savePercentageToDB(roundedValue); */ // Збереження у БД
-  };
 
   return (
     <div className={css.section}>
       <div className={css.radio}>
         <h3 className={css.title}>Today</h3>
-        <ResizableSlider
-          percentage={percentage}
-          onChange={onChange}
-          step={step}
-        />
+        <p className={css.percentage} style={{ left: `${percentage}%` }}>
+          {percentage}%
+        </p>
+        <div className={css.dial}>
+          <div
+            className={css.factpercent}
+            style={{ width: `${percentage}%` }}
+          />
+
+          <div className={css.point} style={{ left: `${percentage}%` }} />
+        </div>
 
         <div className={css.percent}>
           <div className={css.percent_icon}>
