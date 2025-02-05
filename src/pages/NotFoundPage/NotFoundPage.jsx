e65a9f07e8e4e4458b7727011a80d8003e440957
Anton Remezovskyi <antonremezovskyi@gmail.com>
2025-02-05 16:58:57 +0200

    add not found page

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './NotFoundPage.module.css';
+
+const NotFoundPage = () => {
+  return (
+    <div className={css.baseStyle}>
+      <h2 className={css.header}>404 Not Found</h2>
+    </div>
+  );
+};
+
+export default NotFoundPage;
