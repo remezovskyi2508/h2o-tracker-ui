8509be6b95321c5c2d233e793a1cf7bb41d7fe26
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2025-02-05 23:29:09 +0200

    make homePage style need only components & backend

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
 
+import css from './HomePage.module.css';
+
 const HomePage = () => {
   return (
-    <div>
-      <div>HomePage</div>
-      <MonthStatsTable />
-      <TodayWaterList />
+    <div className={css.wrapper}>
+      <div className={css.leftSide}></div>
+      <div className={css.rightSide}>
+        <TodayWaterList />
+        <MonthStatsTable />
+      </div>
     </div>
   );
 };
