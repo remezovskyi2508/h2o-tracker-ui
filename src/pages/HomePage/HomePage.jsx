5017001d2735c62e3d60ac3127e6acecb006ee57
Olena Parakonna <166116539+Parakonna@users.noreply.github.com>
2025-02-07 20:05:35 +0100

    style

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,14 +3,15 @@ import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
 import DailyNorma from '../../components/DailyNorma/DailyNorma.jsx';
 
-
 import css from './HomePage.module.css';
+import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel.jsx';
 
 const HomePage = () => {
   return (
     <div className={css.wrapper}>
       <div className={css.leftSide}>
         <DailyNorma />
+        <WaterRatioPanel />
       </div>
       <div className={css.rightSide}>
         <TodayWaterList />
