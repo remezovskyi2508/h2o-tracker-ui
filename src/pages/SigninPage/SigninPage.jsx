efe36c843a3a52b8fffba3f88db43952e64e537b
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2025-02-10 01:11:59 +0200

    finish styles with SigninupPAge

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,47 +1,35 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
-import AuthForm from "../../components/AuthForm/AuthForm.jsx";
-import css from "./SigninPage.module.css";
+import { selectIsLoggedIn } from '../../redux/auth/selectors.js';
+import AuthForm from '../../components/AuthForm/AuthForm.jsx';
+import css from './SigninPage.module.css';
 
 const SigninPage = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const [error, setError] = useState(null);
 
-  const handleSignin = async (userData) => {
+  const handleSignin = async userData => {
     try {
-      console.log("Signing in with:", userData);
-     
+      console.log('Signing in with:', userData);
     } catch (err) {
       setError(err.message);
     }
   };
 
   return (
-    
-
-    <div className={css.signinPageContainer} >
-    
-     <div className={css.formSection}>
-    
-
-      {!isLoggedIn && (
-        <>
-          {error && <p className={css.error}>{error}</p>}
-          <AuthForm onSubmit={handleSignin} buttonText="Sign in" />
-        
-        </>
-      )}
-      
-
-</div>
-<div className={css.background}>
+    <div className={css.signinPageContainer}>
+      <div className={css.formSection}>
+        {!isLoggedIn && (
+          <>
+            {error && <p className={css.error}>{error}</p>}
+            <AuthForm onSubmit={handleSignin} buttonText="Sign in" />
+          </>
+        )}
+      </div>
+      <div className={css.background}></div>
     </div>
-    </div>
-   
   );
 };
 
 export default SigninPage;
-
