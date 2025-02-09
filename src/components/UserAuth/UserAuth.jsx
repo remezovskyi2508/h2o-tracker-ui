b97b0e6c31a51b173ca783ad4e57e4a6d112473b
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2025-02-09 18:58:39 +0100

    fix-header

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,14 +1,14 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import style from './UserAuth.module.css';
 const UserAuth = () => {
   return (
     <div>
-      <NavLink to="/signin" className={style.singIn}>
+      <Link to="/signin" className={style.singIn}>
         Sign in
         <svg className={style.iconUser} width="28" height="28">
           <use href="/public/images/icons.svg#icon-outline"></use>
         </svg>
-      </NavLink>
+      </Link>
     </div>
   );
 };
