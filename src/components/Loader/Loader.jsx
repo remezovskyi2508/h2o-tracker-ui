3df23ffe7085b07ab8ad0d63cdf5dd1ce6778ace
liddiia <liddabojko@gmail.com>
2025-02-10 21:19:31 +0200

    renew styles sinin-up pages loader add

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
-
+import { Puff } from 'react-loader-spinner';
 const Loader = () => {
-  return <div>Loader</div>;
+  return <div>(<Puff
+    visible={true}
+    height="80"
+    width="80"
+    color="var(--prim-color-blue)"
+    ariaLabel="puff-loading"
+    wrapperStyle={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}
+
+    />)
+  </div>;
 };
 
 export default Loader;
