6325a4f4e42df6bf5ae70f4ffb47f47691e580dd
Angelina Chigiryova <chigiryova31@gmail.com>
2025-02-10 16:59:40 +0200

    changed user logo

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,42 +1,53 @@
 import { useEffect, useState } from 'react';
 import style from './UserLogo.module.css';
-import axios from 'axios';
 import UserLogoModal from '../UserLogoModal/UserLogoModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUserInfo } from '../../redux/user/selectors.js';
+import { selectUserId } from '../../redux/auth/selectors.js';
+import { fetchUserInfo } from '../../redux/user/operations.js';
 
-export const UserLogo = ({ userName, userEmail }) => {
-  const [user, setUser] = useState(null);
+export const UserLogo = () => {
   const [isUserModalOpen, setUserModalOpen] = useState(false);
   const toggleModal = () => {
     setUserModalOpen(prev => !prev);
   };
+  const dispatch = useDispatch();
+  const userData = useSelector(selectUserInfo);
+  const userId = useSelector(selectUserId);
   useEffect(() => {
-    axios
-      .get('/api/user')
-      .then(response => setUser(response.data))
-      .catch(error => console.error('User not found', error));
-  }, []);
-  const firstLetter =
-    userName?.charAt(0).toUpperCase() ||
-    userEmail?.charAt(0).toUpperCase() ||
-    'U';
+    if (userId) {
+      if (userData) {
+        dispatch(fetchUserInfo(userId));
+      }
+    }
+  });
+
   return (
     <>
       <button onClick={toggleModal} type="button" className={style.svgBtn}>
         <div className={style.user}>
-          <span className={style.userName}>{user?.name}</span>
-          {user?.avatar ? (
-            <img src={user.avatar} alt="Avatar" width="28" height="28" />
-          ) : (
-            <div className={style.placeholder}>
-              {user?.name ? user.name[0] : 'User'}
-              <div className={style.avatar}>{firstLetter}</div>
-            </div>
-          )}
-        </div>
-        <div>
-          <svg className={style.icon} width="12" height="7">
-            <use href="/public/images/icons.svg#icon-vector"></use>
-          </svg>
+          <span className={style.userName}>
+            {userData?.name || userData?.email?.split('@')[0] || 'User'}
+          </span>
+          <div>
+            {userData?.avatar ? (
+              <img
+                src={userData?.avatar?.url}
+                alt="User"
+                className={style.avatar}
+              />
+            ) : (
+              <div className={style.initials}>
+                {userData?.name?.[0]?.toUpperCase() ||
+                  userData?.email?.split('@')[0]?.[0]?.toUpperCase()}
+              </div>
+            )}
+          </div>
+          <div>
+            <svg className={style.icon} width="12" height="7">
+              <use href="/images/icons.svg#icon-vector"></use>
+            </svg>
+          </div>
         </div>
       </button>
 
