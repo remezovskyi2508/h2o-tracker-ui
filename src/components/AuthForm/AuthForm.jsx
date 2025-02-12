868b368014983af0dc817760ae3cf4dd9e4bd294
Iryna <mefodievna2908@gmail.com>
2025-02-12 16:21:11 +0200

    push-messages-success

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,13 +1,15 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, useFormikContext } from 'formik';
 import { useDispatch } from 'react-redux';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { login, register } from '../../redux/auth/operations.js';
 import * as Yup from 'yup';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 import css from './AuthForm.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
 
-const AuthSchema = isSignup =>
+
+const AuthSchema = (isSignup) =>
   Yup.object().shape({
     email: Yup.string()
       .email('Invalid email address')
@@ -23,6 +25,21 @@ const AuthSchema = isSignup =>
     }),
   });
 
+
+const ToastErrors = () => {
+  const { errors, submitCount } = useFormikContext();
+
+  useEffect(() => {
+    if (submitCount > 0 && Object.keys(errors).length > 0) {
+      Object.values(errors).forEach((message) => {
+        toast.error(message);
+      });
+    }
+  }, [errors, submitCount]);
+
+  return null;
+};
+
 const AuthForm = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -31,54 +48,59 @@ const AuthForm = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
 
+
   const initialValues = isSignup
     ? { email: '', password: '', repeatPassword: '' }
     : { email: '', password: '' };
 
-    const handleSubmit = async (values, actions) => {
-      try {
-        const { repeatPassword, ...formData } = values;
-        if (isSignup) {
-          await dispatch(register(formData)).unwrap();
-          navigate('/signin');
-        } else {
-          await dispatch(login(formData)).unwrap();
-        }
-        actions.resetForm();
-      } catch (error) {
-       
-        if (error.response && error.response.data && error.response.data.message) {
-         
-          actions.setStatus({ serverError: error.response.data.message });
-        } else {
-       
-        
-          actions.setFieldError('password',  'Password is wrong');
-        }
-        actions.setSubmitting(false);
-      }
-    };
 
- 
+  const handleSubmit = async (values, actions) => {
+    try {
+      const { repeatPassword, ...formData } = values;
+      if (isSignup) {
+        await dispatch(register(formData)).unwrap();
+        toast.success('User is successfully registered');
+        navigate('/signin');
+      } else {
+        await dispatch(login(formData)).unwrap();
+        toast.success('User is successfully logged in');
+      }
+      actions.resetForm(); 
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        actions.setStatus({ serverError: error.response.data.message });
+      } else {
+        actions.setFieldError('password', 'Password is wrong');
+      }
+      actions.setSubmitting(false);
+    }
+  };
+
   return (
     <div className={css.wrapper}>
       <Formik
         initialValues={initialValues}
         validationSchema={AuthSchema(isSignup)}
         onSubmit={handleSubmit}
+        enableReinitialize
       >
-        {({ isSubmitting, errors, touched }) => (
-          <Form className={css.form}>
+        {({ isSubmitting }) => (
+          <Form autoComplete="off" className={css.form}>
+       
+            <ToastErrors />
             <h2 className={css.formTitle}>
               {isSignup ? 'Sign Up' : 'Sign In'}
             </h2>
 
             <label className={css.label}>
-              <span> Enter your email:</span>
+              <span>Enter your email:</span>
               <Field
-                className={`${css.input} ${
-                  errors.email && touched.email ? css.inputError : ''
-                }`}
+                autoComplete="off"
+                className={css.input}
                 type="text"
                 name="email"
                 placeholder="E-mail"
@@ -94,9 +116,8 @@ const AuthForm = () => {
               <span>Enter your password:</span>
               <div className={css.passwordContainer}>
                 <Field
-                  className={`${css.input} ${
-                    errors.password && touched.password ? css.inputError : ''
-                  }`}
+                  autoComplete="off"
+                  className={css.input}
                   type={showPassword ? 'text' : 'password'}
                   name="password"
                   placeholder="Password"
@@ -107,9 +128,9 @@ const AuthForm = () => {
                   onClick={() => setShowPassword(!showPassword)}
                 >
                   {showPassword ? (
-                    <FiEye size={16} color="var(--prim-color-blue)" />
+                    <FiEye size={16} />
                   ) : (
-                    <FiEyeOff size={16} color="var(--prim-color-blue)" />
+                    <FiEyeOff size={16} />
                   )}
                 </button>
               </div>
@@ -125,9 +146,8 @@ const AuthForm = () => {
                 <span>Repeat Password:</span>
                 <div className={css.passwordContainer}>
                   <Field
-                    className={`${css.input} ${
-                      errors.password && touched.password ? css.inputError : ''
-                    }`}
+                    autoComplete="off"
+                    className={css.input}
                     type={showRepeatPassword ? 'text' : 'password'}
                     name="repeatPassword"
                     placeholder="Repeat Password"
@@ -138,9 +158,9 @@ const AuthForm = () => {
                     onClick={() => setShowRepeatPassword(!showRepeatPassword)}
                   >
                     {showRepeatPassword ? (
-                      <FiEye size={16} color="var(--prim-color-blue)" />
+                      <FiEye size={16} />
                     ) : (
-                      <FiEyeOff  size={16} color="var(--prim-color-blue)" />
+                      <FiEyeOff size={16} />
                     )}
                   </button>
                 </div>
@@ -152,11 +172,7 @@ const AuthForm = () => {
               </label>
             )}
 
-            <button
-              className={css.button}
-              type="submit"
-              disabled={isSubmitting}
-            >
+            <button className={css.button} type="submit" disabled={isSubmitting}>
               {isSignup ? 'Sign Up' : 'Sign In'}
             </button>
 
